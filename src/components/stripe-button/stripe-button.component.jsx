a85c88c2b383ef761b2f2eb3bc6c3a53491a56e8
Farid Mohamadi <faridmohamady1@yahoo.com>
2020-11-14 23:08:34 +0330

    updated the title and favicon integrated stripe for test payments

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51HnU7OKxaScOfD6LhK8v7OibbzCSt4nXGr9OvAGqeaLyOcrCeEIW4Ara99vAve505SJwPi9bCrrO200GLYqzqILx00Ils3iQUY';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name="Farid's Clothing"
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
